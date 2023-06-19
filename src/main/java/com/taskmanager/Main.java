ff8965b93f931e96f606ce42e3c985f01fc6741b
groszmarti <grosz.marti@hotmail.com>
2023-06-19 11:10:50 +0200

    set up database connection and table creation

--- src/main/java/com/taskmanager/Main.java
+++ src/main/java/com/taskmanager/Main.java
@@ -1,4 +1,4 @@
-package org.example;
+package com.taskmanager;
 
 public class Main {
     public static void main(String[] args) {
---
+++ src/main/java/com/taskmanager/initialize_tables/TableStatements.java
@@ -0,0 +1,5 @@
+package com.taskmanager.initialize_tables;
+
+public interface TableStatements {
+    String TASK = "CREATE TABLE task (id SERIAL PRIMARY KEY, title character varying, description character varying, due_date date, priority_level character varying, status character varying);";
+}
