2af5ef5dbcd2bc9fad034e60e12ab1ca9594681b
arvaibank <arvaibank2001@gmail.com>
2023-07-10 23:29:54 +0200

    +create full working CRUD

--- src/main/java/com/taskmanager/initialize_tables/TableStatements.java
+++ src/main/java/com/taskmanager/initialize_tables/TableStatements.java
@@ -1,5 +1,7 @@
 package com.taskmanager.initialize_tables;
 
 public interface TableStatements {
-    String TASK = "CREATE TABLE IF EXIST task (id SERIAL PRIMARY KEY, title character varying, description character varying, due_date date, priority_level character varying, status character varying);";
+    String TASK = "CREATE TABLE [IF NOT EXISTS] task (id SERIAL PRIMARY KEY, title character varying, description character varying, due_date date, priority_level character varying, status " +
+            "character " +
+            "varying);";
 }
